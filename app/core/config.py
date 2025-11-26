b107a6e2174ee5752ce74428f4f7fa57f8efffdd
suyeon0131@ajou.ac.kr <kdl111438@gmail.com>
2025-11-26 21:31:41 +0900

    feat: refresh token

--- app/core/config.py
+++ app/core/config.py
@@ -10,7 +10,8 @@ class Settings(BaseSettings):
     # JWT 설정
     JWT_SECRET_KEY: str
     ALGORITHM: str = "HS256"
-    ACCESS_TOKEN_EXPIRE_MINUTES: int = 60 * 24 * 7  # 7일
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 60  # 60분
+    REFRESH_TOKEN_EXPIRE_MINUTES: int = 60 * 24 * 7 # 7일
 
 # 설정 객체 인스턴스 생성
 settings = Settings()
--- app/db/schemas/user.py
+++ app/db/schemas/user.py
@@ -44,3 +44,4 @@ class UserLogin(BaseModel):
 class Token(BaseModel):
     access_token: str
     token_type: str
+    refresh_token: str
