6dd8e41a4ec90104ad88c5b370d6a0ddb5150e2e
suyeon0131@ajou.ac.kr <kdl111438@gmail.com>
2025-11-28 14:28:14 +0900

    feat: 이미지 업로드 및 정적 파일 서빙

--- app/api/v1/api_router.py
+++ app/api/v1/api_router.py
@@ -1,8 +1,9 @@
 from fastapi import APIRouter
-from app.api.v1.endpoints import auth, posts, users
+from app.api.v1.endpoints import auth, posts, users, images
 
 api_router = APIRouter()
 
 api_router.include_router(auth.router, prefix="/auth", tags=["auth"])
 api_router.include_router(posts.router, prefix="/posts", tags=["posts"])
 api_router.include_router(users.router, prefix="/users", tags=["users"])
+api_router.include_router(images.router, prefix="/images", tags=["images"])
--- app/main.py
+++ app/main.py
@@ -1,10 +1,13 @@
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 from app.api.v1.api_router import api_router
 
 app = FastAPI(
     title="ABS (Aolda Blog Service) API",
     swagger_ui_parameters={"persistAuthorization": True} # 임시 (토큰 기억)
-    )
+)
+
+app.mount("/uploads", StaticFiles(directory="uploads"), name="uploads")
 
 app.include_router(api_router, prefix="/api/v1")
 
