a72eea18f736a1a604b9268b2fee609fb92ebed3
asitis <asitisdev@gmail.com>
2026-03-10 16:25:24 +0900

    feat: CORS 미들웨어 개선

--- app/core/config.py
+++ app/core/config.py
@@ -1,19 +1,18 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
+
 class Settings(BaseSettings):
-    model_config = SettingsConfigDict(
-        env_file=".env",
-        env_file_encoding="utf-8",
-        extra='ignore')
+    model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8", extra="ignore")
 
     DATABASE_URL: str
 
-    # JWT 설정
+    # Session / JWT 설정
+    SESSION_SECRET_KEY: str
     JWT_SECRET_KEY: str
     ALGORITHM: str = "HS256"
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 60  # 60분
-    REFRESH_TOKEN_EXPIRE_MINUTES: int = 60 * 24 * 7 # 7일
-    
+    REFRESH_TOKEN_EXPIRE_MINUTES: int = 60 * 24 * 7  # 7일
+
     # OAuth / OIDC
     GOOGLE_CLIENT_ID: str
     GOOGLE_CLIENT_SECRET: str
@@ -23,6 +22,14 @@ class Settings(BaseSettings):
     KEYCLOAK_CLIENT_ID: str
     KEYCLOAK_CLIENT_SECRET: str
     API_SERVER_URL: str = "https://blog-api.aoldacloud.com"
-    CONSOLE_PAGE_URL: str = "https://blog.aoldacloud.com"
+    CONSOLE_PAGE_URL: str = "https://abs.aoldacloud.com"
+    FRONTEND_URL: str = "https://blog.aoldacloud.com"
+
+    @property
+    def cors_allow_origins(self) -> list[str]:
+        console_page_url = self.CONSOLE_PAGE_URL.strip()
+        frontend_url = self.FRONTEND_URL.strip()
+        return [console_page_url, frontend_url]
+
 
 settings = Settings()
--- app/main.py
+++ app/main.py
@@ -1,27 +1,28 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
 from app.api.v1.api_router import api_router
+from app.core.config import settings
 from starlette.middleware.sessions import SessionMiddleware
-from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(
     title="ABS (Aolda Blog Service) API",
-    swagger_ui_parameters={"persistAuthorization": True} # 임시 (토큰 기억)
+    swagger_ui_parameters={"persistAuthorization": True},  # 임시 (토큰 기억)
 )
 
-app.add_middleware(SessionMiddleware, secret_key="some_random_secret_string")
+app.add_middleware(SessionMiddleware, secret_key=settings.SESSION_SECRET_KEY)
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=settings.cors_allow_origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
-
 app.mount("/uploads", StaticFiles(directory="uploads"), name="uploads")
 
 app.include_router(api_router, prefix="/api/v1")
+
 
 @app.get("/")
 def read_root():
